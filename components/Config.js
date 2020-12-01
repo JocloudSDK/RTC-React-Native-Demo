182122e71dd8d75d73c764d31e337f8608574df4
can <767314091@qq.com>
2020-12-01 15:24:55 +0800

    commit demo

--- components/Config.js
+++ components/Config.js
@@ -3,12 +3,12 @@ export default class Config {
     //todo add your add id here
     static APPID =
 
-    static uid = '1';
-    static channelId = '1';
+    static uid = '1'
+    static channelId = '1'
 
-    static STORAGE_UID_KEY = "STORAGE_UID_KEY";
-    static STORAGE_CHANNEL_ID_KEY = "STORAGE_ROOM_ID_KEY";
-    static STORAGE_APP_ID_KEY = "STORAGE_APP_ID_KEY";
+    static STORAGE_UID_KEY = "STORAGE_UID_KEY"
+    static STORAGE_CHANNEL_ID_KEY = "STORAGE_ROOM_ID_KEY"
+    static STORAGE_APP_ID_KEY = "STORAGE_APP_ID_KEY"
 
     static STOP_ON_DEVICE_STATS_LOG = "1"
     static STOP_ON_SDK_AUTH_RESULT_LOG = "1"
